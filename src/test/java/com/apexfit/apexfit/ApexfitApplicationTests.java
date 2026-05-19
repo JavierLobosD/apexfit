80c70a7480d5b855cac9751123363fdf9bca8202
JavierLobosD <ja.lobosr@duocuc.cl>
2026-05-18 22:12:24 -0400

    estructura general del proyecto

---
+++ src/test/java/com/apexfit/apexfit/ApexfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexfit.apexfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
